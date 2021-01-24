835d8de9817a4e21d5453814225a2b5389787397
shoogkabiya <shoogkabiya@hotmail.com>
2021-01-24 13:31:00 +0200

    pull changes

--- database/models/exercise.js
+++ database/models/exercise.js
@@ -6,9 +6,9 @@ const Schema = moongose.Schema;
 const ExerciseSchema = new Schema({
   name: String,
   iamges: [ImagesSchema],
-  questions: { type: moongose.Schema.Types.ObjectId, ref: QuestionSchema },
+  questions: { type: moongose.Schema.Types.ObjectId, ref: QuestionSchema }, //here, the  ExerciseSchema  should be imported above (when its done)
 });
-
+//creating exercise model
 const exercise = moongose.model("exercise", ExerciseSchema);
 
 module.exports = exercise;
