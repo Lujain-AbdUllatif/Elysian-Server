7c82f36009d26772f2f43905ea560fcadb9fa682
OmklthomAmara <amaraomklthom@gmail.com>
2021-01-24 13:31:10 +0200

    server + connecting to database + routers

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT;
+const Connection = require("./database/connection");
+Connection;
+
+//const examineeRouter = require("./routers/examineeRouter");
+//const testerRouter = require("./routers/testerRouter");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+//app.all(/examinee/, authExaminee, examineeRouter);
+//app.all(/tester/,authTester, testerRouter);
+
+app.listen(PORT, () => console.log(`Server started`));
