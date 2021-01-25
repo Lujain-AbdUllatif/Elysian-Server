3a517e893fc3edd12a38704ced8596f9e76be3e1
shoogkabiya <shoogkabiya@hotmail.com>
2021-01-25 20:39:08 +0200

    pull from main

--- handlers/tester/addTest.js
+++ handlers/tester/addTest.js
@@ -8,12 +8,11 @@ const addTest = async (req, res, next) => {
 
   try {
     test = await saveTest.save();
-    // console.log("test1:", test);
+
     res.status(200).json(test);
   } catch (err) {
     err.status = 400;
-    // return next(err);
-    console.log(err);
+    return next(err);
   }
 };
 module.exports = addTest;
