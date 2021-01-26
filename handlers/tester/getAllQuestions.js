7430908b6aee0e5f12675e12e50a150afb6b7760
Muhammad Awwad <hammode.awad@gmail.com>
2021-01-27 00:10:51 +0200

    done with getAllQuestions handler

--- handlers/tester/getAllQuestions.js
+++ handlers/tester/getAllQuestions.js
@@ -1,15 +1,22 @@
 //requiring models
+const { find } = require("../../database/models/exercise");
 const Exercise = require("../../database/models/exercise");
 const Question = require("../../database/models/question");
 
 const getAllQuestions = async (req, res) => {
   const { exerciseid } = req.params;
+
   try {
     const exercise = await Exercise.findOne({ _id: exerciseid });
-    const questions = exercies.questions.map(async (questionId) => {
-      return await Question.findOne({ _id: questionId });
+    console.log("exercise is: ", exercise);
+    const questionsIdsArr = exercise.questions;
+    const questionsRecords = await Question.find({
+      _id: {
+        $in: questionsIdsArr,
+      },
     });
-    res.status(200).json(questions);
+    console.log("questions records: ", questionsRecords);
+    res.status(200).json(questionsRecords);
   } catch (err) {
     res.status(500).json("server error");
   }
