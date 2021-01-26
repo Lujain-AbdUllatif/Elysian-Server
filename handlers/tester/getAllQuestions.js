ef24ce2d361545225aee1bfb04ee787f736a8cef
Muhammad Awwad <hammode.awad@gmail.com>
2021-01-26 12:54:54 +0200

    no changes

---
+++ handlers/tester/getAllQuestions.js
@@ -0,0 +1,18 @@
+//requiring models
+const Exercise = require("../../database/models/exercise");
+const Question = require("../../database/models/question");
+
+const getAllQuestions = async (req, res) => {
+  const { exerciseid } = req.params;
+  try {
+    const exercise = await Exercise.findOne({ _id: exerciseid });
+    const questions = exercise.questions.map(async (questionId) => {
+      return await Question.findOne({ _id: questionId });
+    });
+    res.status(200).json(questions);
+  } catch (err) {
+    res.status(500).json("server error");
+  }
+};
+
+module.exports = getAllQuestions;
