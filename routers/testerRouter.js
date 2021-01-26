d1b907b5f4c002438b4130255b31728c6f8f8cfe
Muhammad Awwad <hammode.awad@gmail.com>
2021-01-26 11:50:37 +0200

    getAllQuestion

--- routers/testerRouter.js
+++ routers/testerRouter.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const signup = require("../handlers/tester/signup");
 const addTest = require("../handlers/tester/addTest");
 const getTests = require("../handlers/tester/getTests");
+const getAllQuestions = require("../handlers/tester/getAllQuestions");
 //routes
 router.post("/tester/signup", signup);
 router.post("/tester/test", addTest);
 router.get("/tester/test", getTests);
+router.get("/tester/questions/:exerciseid", getAllQuestions);
 module.exports = router;
