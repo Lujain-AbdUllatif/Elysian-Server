794a9cbc1ab93ee69f3b3544cf5f1f5b187207b6
Lujain <lujain.abdallatif@mail.huji.ac.il>
2021-01-27 10:55:32 +0200

    commenting stuff

--- handlers/tester/getExaminees.js
+++ handlers/tester/getExaminees.js
@@ -2,14 +2,18 @@ const getExaminees = (req, res, next) => {};
 
 module.exports = getExaminees;
 
-testers
-  .find({
-    _id: {
-      $in: [
-        mongoose.Types.ObjectId("600ebe0746626c3d6a99346f"),
-        mongoose.Types.ObjectId("600ec3c4215ff9441944058a"),
-        mongoose.Types.ObjectId("600ec97cfaa9a8491eef6dd6"),
-      ],
-    },
-  })
-  .pretty();
+// testers.find({_id: {$in:
+//     [mongoose.Types.ObjectId("600ebe0746626c3d6a99346f"),
+//     mongoose.Types.ObjectId("600ec3c4215ff9441944058a"),
+//     mongoose.Types.ObjectId("600ec97cfaa9a8491eef6dd6")
+//     ,],},}).pretty();
+
+// db.testers.find({'_id': { $in: [
+//     ObjectId("600ebe0746626c3d6a99346f"),
+//     ObjectId("600ec3c4215ff9441944058a"),
+//     ObjectId("600ec97cfaa9a8491eef6dd6")
+// ]}}).pretty();
+
+// db.testers.find({'_id': { $in: [ObjectId("600ebe0746626c3d6a99346f"),ObjectId("600ec3c4215ff9441944058a"),ObjectId("600ec97cfaa9a8491eef6dd6")]}}).pretty()
+
+// db.testers.find({'_id':"00ec3c4215ff9441944058a"})
