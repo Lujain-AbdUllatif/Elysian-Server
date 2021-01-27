35bd735f55a5f8358a6a91064a7055b113548ff2
shoogkabiya <shoogkabiya@hotmail.com>
2021-01-27 15:12:20 +0200

    pull from main

--- handlers/tester/addTest.js
+++ handlers/tester/addTest.js
@@ -1,20 +1,25 @@
 const Test = require("../../database/models/test");
 
 const addTest = async (req, res, next) => {
-  const saveTest = new Test({
-    name: req.body.name,
-    time: req.body.time,
-    Upcoming_test: req.body.Upcoming_test,
-    exercises: req.idsArray,
-  });
+  const { name, time, exercises } = req.body;
+  if (name && time && exercises) {
+    const saveTest = new Test({
+      name,
+      time,
+      exercises,
+    });
 
-  try {
-    test = await saveTest.save();
+    try {
+      test = await saveTest.save();
+      console.log(test);
 
-    res.status(200).json(test);
-  } catch (err) {
-    err.status = 400;
-    return next(err);
+      res.status(200).json(test);
+    } catch (err) {
+      err.status = 400;
+      return next(err);
+    }
+  } else {
+    res.status(400).send("missing fields");
   }
 };
 module.exports = addTest;
